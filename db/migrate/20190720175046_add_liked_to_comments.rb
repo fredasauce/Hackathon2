5cf974823595fd84a4584602a7747fc5def48011
Alfreda Tsai <alfredatsai@gmail.com>
2019-07-20 12:40:11 -0600

    deleted liked from comment, added to video, seeds file successful

--- db/migrate/20190720175046_add_liked_to_comments.rb
+++
@@ -1,5 +0,0 @@
-class AddLikedToComments < ActiveRecord::Migration[5.2]
-  def change
-    add_column :comments, :liked, :boolean
-  end
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,17 +4,17 @@
   name = Faker::Name.name
   email = Faker::Internet.email
   password = "password"
-  avatar = Faker::Avatar.image(name, "300x300", "png", "set5"),
-  user = User.create(name: name, email: email, password: password, password_confirmation: password, avatar: avatar)
+  image = Faker::Avatar.image(name, "300x300", "png", "set5"),
+  user = User.create(name: name, email: email, password: password, password_confirmation: password, image: image)
 
   5.times do
     vid = Video.create(
       title: Faker::Cannabis.health_benefit,
       duration: "00:38",
       genre: Faker::Book.genre,
-      description: Faker::FamousLastWords.last_words,
+      description: Faker::Quote.famous_last_words,
       trailer: Faker::LoremFlickr.image,
-      liked: @liked_posts.sample,
+      liked: @liked_video.sample,
       user_id: user.id
     )
 
@@ -25,7 +25,9 @@
         )
 
       end
-      
+
+  end
+
 end
 
 puts "5 Users Created, each with 5 videos, each video with 5 comments."
