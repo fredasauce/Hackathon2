437c24055f33f219fe85d3192418ec7c04821e81
Alfreda Tsai <alfredatsai@gmail.com>
2019-07-20 12:23:02 -0600

    added seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,30 +1,31 @@
-@liked_posts = [ true, false ]
+@liked_video = [ true, false ]
 
 5.times do
   name = Faker::Name.name
   email = Faker::Internet.email
   password = "password"
-  user = User.create(name: name, email: email, password: password, password_confirmation: password)
+  avatar = Faker::Avatar.image(name, "300x300", "png", "set5"),
+  user = User.create(name: name, email: email, password: password, password_confirmation: password, avatar: avatar)
 
-  vid = Video.create(
-    name: user.name,
-    gender: Faker::Gender.type,
-    birthday: Faker::Date.birthday(min_age = 18),
-    avatar: Faker::Avatar.image(name, "300x300", "png", "set5"),
-    user_id: user.id
-  )
+  5.times do
+    vid = Video.create(
+      title: Faker::Cannabis.health_benefit,
+      duration: "00:38",
+      genre: Faker::Book.genre,
+      description: Faker::FamousLastWords.last_words,
+      trailer: Faker::LoremFlickr.image,
+      liked: @liked_posts.sample,
+      user_id: user.id
+    )
 
-    5.times do
-      Post.create(
-        author: Faker::Name.name,
-        body: Faker::Lorem.paragraph_by_chars(256, false),
-        liked: @liked_posts.sample,
-        account_id: acct.id 
-      )
+      5.times do
+        Comment.create(
+          body: Faker::Lorem.paragraph_by_chars(256, false),
+          video_id: vid.id 
+        )
 
-    end
-
-
+      end
+      
 end
 
-puts "20 Users with an Account Created, each Acct with 5 posts"
+puts "5 Users Created, each with 5 videos, each video with 5 comments."
