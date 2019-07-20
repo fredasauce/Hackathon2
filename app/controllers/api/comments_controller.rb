b2acba7e316e58981fce2976e9a5c9fb3c53f991
Peter Mackay <project2501@gmail.com>
2019-07-20 11:49:32 -0600

    video controller

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -1,17 +1,3 @@
 class Api::CommentsController < ApplicationController
-  def index
-    render json: Comment.all
-  end
-
-  def show
-  end
-
-  def create
-  end
-
-  def update
-  end
-
-  def destroy
-  end
+ 
 end
