a8c87c2949772432dfbdc8e5028c773ce79ddbb5
Aaron Krishnan <buzzkri@gmail.com>
2019-07-20 11:07:14 -0700

    added video page

---
+++ client/src/components/Video.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect, } from "react";
+import { Card, Segment, } from "semantic-ui-react";
+import styled from "styled-components"
+
+const Video = () => {
+  const {state, setState} = useState('');
+  
+  return(
+
+  )
+}
